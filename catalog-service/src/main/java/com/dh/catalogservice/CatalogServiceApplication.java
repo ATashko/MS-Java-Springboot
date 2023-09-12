e700b2c851a621bdbdf40fb6954372aef2002ef9
ATashko <tashkomendez@gmail.com>
2023-09-11 23:34:33 -0500

    zipkin done

--- catalog-service/src/main/java/com/dh/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/dh/catalogservice/CatalogServiceApplication.java
@@ -2,8 +2,11 @@ package com.dh.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 
 @EnableDiscoveryClient
@@ -14,5 +17,8 @@ public class CatalogServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CatalogServiceApplication.class, args);
     }
-
+    @Bean
+    public RestTemplate restTemplate (RestTemplateBuilder builder){
+        return builder.build();
+    }
 }
--- serie-service/src/main/java/com/example/serieservice/SerieServiceApplication.java
+++ serie-service/src/main/java/com/example/serieservice/SerieServiceApplication.java
@@ -5,8 +5,10 @@ import com.example.serieservice.repository.SerieRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,6 +20,10 @@ public class SerieServiceApplication {
         SpringApplication.run(SerieServiceApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate (RestTemplateBuilder builder){
+        return builder.build();
+    }
     @Bean
     public CommandLineRunner loadData(SerieRepository repository) {
         String baseUrl = "www.netflix.com/series";
